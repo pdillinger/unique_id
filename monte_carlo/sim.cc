8decbb29b239999ff45c588b5172faa253def256
Peter Dillinger <peterd@fb.com>
2021-09-10 21:08:51 -0700

    Clean up, add multi-xor

--- monte_carlo/sim.cc
+++ monte_carlo/sim.cc
@@ -1,9 +1,10 @@
+#include <stdlib.h>
 #include <cstdint>
 #include <iostream>
 #include <cstring>
 #include <random>
 
-void Go() {
+void Go(size_t count) {
   std::mt19937_64 r{std::random_device()()};
 
   constexpr uint64_t kMaxIter = 100000;
@@ -15,36 +16,42 @@ void Go() {
     kRandom,
     kAddition,
     kXor,
-    kMultAddition,
-    kMultXor,
+    kMultiXor,
+    kMultiplyAddition,
+    kMultiplyXor,
   };
 
   std::vector<bool> v;
-  for (auto approach : {kRandom, kAddition, kXor, kMultAddition, kMultXor}) {
+  for (auto approach : {kRandom, kAddition, kXor, kMultiXor, kMultiplyAddition, kMultiplyXor}) {
     uint64_t threshold_total = 0;
     for (uint64_t iter = 0; iter < kMaxIter; ++iter) {
       v.assign(kSize, false);
       bool collision = false;
       do {
         size_t start = static_cast<size_t>(r()) & kMask;
-        size_t count;
-        if (approach == kRandom) {
-          count = 1;
-        } else {
-          count = 1000;
-        }
         for (size_t i = 0; i < count && !collision; ++i) {
           size_t pos;
           size_t mult_offset = static_cast<size_t>(
               (i * uint64_t{0xf01f39c13d6a7d81}) >> (64 - kBits));
-          if (approach == kAddition) {
+          switch (approach) {
+          case kRandom:
+            pos = static_cast<size_t>(r()) & kMask;
+            break;
+          case kAddition:
             pos = (start + i) & kMask;
-          } else if (approach == kMultAddition) {
-            pos = (start + mult_offset) & kMask;
-          } else if (approach == kMultXor) {
-            pos = start ^ mult_offset;
-          } else {
+            break;
+          case kXor:
             pos = start ^ i;
+            break;
+          case kMultiXor:
+            pos = start ^ (i % 20) ^ ((i / 20) << 8);
+            break;
+          case kMultiplyAddition:
+            pos = (start + mult_offset) & kMask;
+            break;
+          case kMultiplyXor:
+            pos = start ^ mult_offset;
+            break;
           }
           if (v[pos]) {
             collision = true;
@@ -57,19 +64,21 @@ void Go() {
     }
     const char *name = approach == kAddition       ? "addition"
                        : approach == kXor          ? "xor"
-                       : approach == kMultAddition ? "multaddition"
-                       : approach == kMultXor      ? "multxor"
+                       : approach == kMultiXor      ? "multixor"
+                       : approach == kMultiplyAddition ? "multiply-addition"
+                       : approach == kMultiplyXor      ? "multiply-xor"
                                                    : "random";
     std::cerr << "Average threshold with " << name << ": "
               << 1.0 * threshold_total / kMaxIter << " / "
               << kSize << std::endl;
   }
   // Example output with non-random count=1000:
-  // Average threshold with random: 1283.67 / 1048576
-  // Average threshold with addition: 28653.2 / 1048576
-  // Average threshold with xor: 39762.9 / 1048576
-  // Average threshold with multaddition: 20369.2 / 1048576
-  // Average threshold with multxor: 2810.48 / 1048576
+  // Average threshold with random: 1282.94 / 1048576
+  // Average threshold with addition: 28684 / 1048576
+  // Average threshold with xor: 39829.2 / 1048576
+  // Average threshold with multixor: 27957.5 / 1048576
+  // Average threshold with multiply-addition: 20409.5 / 1048576
+  // Average threshold with multiply-xor: 2807.11 / 1048576
   //
   // Winner is xor because there's lowest chance of any overlap, but addition
   // is close. Multiplicative hash + addition is within a factor of two, or
@@ -78,13 +87,18 @@ void Go() {
   // expect from the Birthday paradox.
   //
   // Example output with non-random count=50:
-  // Average threshold with random: 1284.02 / 1048576
-  // Average threshold with addition: 6460.97 / 1048576
-  // Average threshold with xor: 8001.48 / 1048576
-  // Average threshold with multaddition: 4665.97 / 1048576
-  // Average threshold with multxor: 2057.52 / 1048576
+  // Average threshold with random: 1282.83 / 1048576
+  // Average threshold with addition: 6448.52 / 1048576
+  // Average threshold with xor: 8003.43 / 1048576
+  // Average threshold with multixor: 5857.4 / 1048576
+  // Average threshold with multiply-addition: 4668.4 / 1048576
+  // Average threshold with multiply-xor: 2061.64 / 1048576
 }
 
-int main(int /*argc*/, char */*argv*/[]) {
-  Go();
+int main(int argc, char *argv[]) {
+  size_t count = 1000;
+  if (argc > 1) {
+    count = static_cast<size_t>(atoi(argv[1]));
+  }
+  Go(count);
 }
