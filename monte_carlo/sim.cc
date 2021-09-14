62fbf8eb7102a5b77777dd9a7c668994cbf2e11f
Peter Dillinger <peterd@fb.com>
2021-09-13 22:49:46 -0700

    More robust pseudorandomness, probability mode

--- monte_carlo/sim.cc
+++ monte_carlo/sim.cc
@@ -1,13 +1,47 @@
 #include <stdlib.h>
+#include <array>
 #include <cstdint>
 #include <iostream>
 #include <cstring>
 #include <random>
 
-void Go(size_t count) {
-  std::mt19937_64 r{std::random_device()()};
 
-  constexpr uint64_t kMaxIter = 100000;
+uint64_t MurmurHash64A ( const uint64_t * data, int len, uint64_t seed )
+{
+  const uint64_t m = 0xc6a4a7935bd1e995;
+  const int r = 47;
+
+  uint64_t h = seed ^ (len * 8 * m);
+
+  const uint64_t * end = data + len;
+
+  while(data != end)
+  {
+    uint64_t k = *data++;
+
+    k *= m;
+    k ^= k >> r;
+    k *= m;
+
+    h ^= k;
+    h *= m;
+  }
+
+  h ^= h >> r;
+  h *= m;
+  h ^= h >> r;
+
+  return h;
+}
+
+uint64_t random64() {
+  static std::random_device r1;
+  static std::mt19937_64 r2{r1()};
+  std::array<uint64_t, 4> vals = {r1(), r2(), r1(), r2()};
+  return MurmurHash64A(vals.data(), vals.size(), 12345);
+}
+
+void Go(size_t count, int players, size_t iters) {
   constexpr int kBits = 20;
   constexpr size_t kSize = size_t{1} << kBits;
   constexpr size_t kMask = kSize - 1;
@@ -23,19 +57,21 @@ void Go(size_t count) {
 
   std::vector<bool> v;
   for (auto approach : {kRandom, kAddition, kXor, kMultiXor, kMultiplyAddition, kMultiplyXor}) {
-    uint64_t threshold_total = 0;
-    for (uint64_t iter = 0; iter < kMaxIter; ++iter) {
+    size_t threshold_total = 0;
+    size_t collision_iters = 0;
+    for (uint64_t iter = 0; iter < iters; ++iter) {
       v.assign(kSize, false);
       bool collision = false;
-      do {
-        size_t start = static_cast<size_t>(r()) & kMask;
+      uint64_t ids = 0;
+      for (int p = 0; !collision && (players < 0 || p < players); ++p) {
+        size_t start = static_cast<size_t>(random64()) & kMask;
         for (size_t i = 0; i < count && !collision; ++i) {
           size_t pos;
           size_t mult_offset = static_cast<size_t>(
               (i * uint64_t{0xf01f39c13d6a7d81}) >> (64 - kBits));
           switch (approach) {
           case kRandom:
-            pos = static_cast<size_t>(r()) & kMask;
+            pos = static_cast<size_t>(random64()) & kMask;
             break;
           case kAddition:
             pos = (start + i) & kMask;
@@ -55,12 +91,14 @@ void Go(size_t count) {
           }
           if (v[pos]) {
             collision = true;
+            ++collision_iters;
+            threshold_total += ids;
           } else {
             v[pos] = true;
-            ++threshold_total;
+            ++ids;
           }
         }
-      } while (!collision);
+      }
     }
     const char *name = approach == kAddition       ? "addition"
                        : approach == kXor          ? "xor"
@@ -68,9 +106,14 @@ void Go(size_t count) {
                        : approach == kMultiplyAddition ? "multiply-addition"
                        : approach == kMultiplyXor      ? "multiply-xor"
                                                    : "random";
-    std::cerr << "Average threshold with " << name << ": "
-              << 1.0 * threshold_total / kMaxIter << " / "
-              << kSize << std::endl;
+    if (players < 0) {
+      std::cerr << "Average threshold with " << name << ": "
+                << 1.0 * threshold_total / iters << " / "
+                << kSize << std::endl;
+    } else {
+      std::cerr << "Collision probability " << name << ": "
+                << 1.0 * collision_iters / iters << std::endl;
+    }
   }
   // Example output with non-random count=1000:
   // Average threshold with random: 1282.94 / 1048576
@@ -97,8 +140,16 @@ void Go(size_t count) {
 
 int main(int argc, char *argv[]) {
   size_t count = 1000;
+  int players = 10;
+  size_t iters = 10000;
   if (argc > 1) {
     count = static_cast<size_t>(atoi(argv[1]));
   }
-  Go(count);
+  if (argc > 2) {
+    players = atoi(argv[2]);
+  }
+  if (argc > 3) {
+    iters = static_cast<size_t>(atoi(argv[3]));
+  }
+  Go(count, players, iters);
 }
